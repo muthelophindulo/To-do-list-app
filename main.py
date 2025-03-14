46c3a06530afedd8123078fd872f6cd7ae722fd0
Phindulo.dev <95999814+muthelophindulo@users.noreply.github.com>
2025-03-14 09:45:07 +0200

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from todo import *
+from App import *
 
 print("."*5,"welcome","."*5)
 start = input("would you like to open the app?: ")
 
 if start == "yes" or start == "y":
-    run()
+    app()
 else:
     print("error")
+    option = input("would you like to try again? ")
+    if option == "yes" or option == "y":
+        app()
+    else:
+        print("goodbye")
