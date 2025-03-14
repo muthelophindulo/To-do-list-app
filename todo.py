1bba97ec7bf18cb9bdbc0ddb07ccfdfb0efb261f
Phindulo.dev <95999814+muthelophindulo@users.noreply.github.com>
2025-03-14 09:46:08 +0200

    App.py

--- todo.py
+++ todo.py
@@ -1,9 +1,10 @@
 from manipulate_list import *
+import time
 
-def run():
-    print("........TO DO LIST APP.........")
+def app():
+    print("..............TO DO LIST APP..............")
     while True:
-
+        time.sleep(0.2)
         print("""
             1. view the to do list
             2. add item
@@ -11,34 +12,42 @@ def run():
             4. mark done
             5. view previous done 
                   """)
+        time.sleep(0.2)
 
         inp = int(input("what would you like to do? "))
         match inp:
             case 1:
                 view_list()
+                time.sleep(0.2)
             case 2:
                 item = input("enter item to be added: ")
                 add_item(item)
                 print()
+                time.sleep(0.2)
 
-                print(f"succesfully added {inp} on the to do list")
+                print(f"succesfully added {item} on the to do list")
+
                 while True:
                     continuity = input("would you like to add another item?(y/n): ")
+
                     if continuity == "y":
                         item = input("enter item to be added: ")
                         add_item(item)
                         print()
                         print(f"succesfully added {inp} on the to do list") 
+
                     else:
-                        break           
+                        break   
+
             case 3:
-                pass
+                view_list()
+                remove()
             
             case 4:
-                pass
+                mark_done()
             
             case 5:
-                pass
+                view_prev_done()
             
             case _:
                 print("error")
@@ -47,5 +56,6 @@ def run():
         if cont == "y" or cont == "yes":
             continue
         else:
+            time.sleep(0.2)
             print("Goodbye")
             break
