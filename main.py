dd470f85f1afffa55de9502c21c8479aee8e1688
Phindulo.dev <95999814+muthelophindulo@users.noreply.github.com>
2025-04-12 22:31:32 +0200

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 from App import *
 
-print("."*5,"welcome","."*5)
-start = input("would you like to open the app?: ")
-
-if start == "yes" or start == "y":
-    app()
-else:
-    print("error")
-    option = input("would you like to try again? ")
-    if option == "yes" or option == "y":
-        app()
-    else:
-        print("goodbye")
+application.database()
